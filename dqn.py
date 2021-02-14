dbe0ad18c01cb340a447f03890a15c0ff6b968fc
Teddy Holappa <teddyholappa@Teddys-MBP.lan>
2021-02-14 00:04:22 -0800

    adding layer

--- dqn.py
+++ dqn.py
@@ -11,16 +11,22 @@ class DeepQNetwork(nn.Module):
 	    self.fc1_dims = fc1_dims
 	    self.fc2_dims = fc2_dims
 	    self.n_actions = n_actions
+	  
 	    self.fc1 = nn.Linear(*self.input_dims, self.fc1_dims)
 	    self.fc2 = nn.Linear(self.fc1_dims, self.fc2_dims)
-	    self.fc3 = nn.Linear(self.fc2_dims, self.n_actions)
+	    self.fc3 = nn.Linear(self.fc2_dims, 256)
+	    self.fc3 = nn.Linear(256, 256)
+	    self.fc4 = nn.Linear(256, self.n_actions)
+	    
 	    self.optimizer = optim.Adam(self.parameters(), lr=lr)
 	    self.loss = nn.MSELoss()
 	    self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
 	    self.to(self.device)
 
 	def forward(self, state):
-	    x = F.relu(self.fc1(state))
-	    x = F.relu(self.fc2(x))
-	    actions = self.fc3(x)
-	    return actions
+		state = state.float()
+		x = F.relu(self.fc1(state)).float()
+		x = F.relu(self.fc2(x)).float()
+		x = F.relu(self.fc3(x)).float()
+		actions = self.fc4(x)
+		return actions
