4fe5bab350654c7153ea6c04adeae829d943d12d
Teddy Holappa <teddyholappa@Teddys-MBP.lan>
2021-02-13 23:49:18 -0800

    adding Deep Q Network and better agent

--- main.py
+++ main.py
@@ -1,25 +1,42 @@
 from game import Game 
 import numpy as np
-import random
-import sys
+from q_agent import Agent
+import os
+import matplotlib.pyplot as plt
 
-inputs = ['w','a','s','d']
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
-def main():
-	game = Game()
-	game.print_board()
-	while(True):
-		#change this to input() if you would like to play yourself.
-		user_input = inputs[random.randint(0,3)]
-		direction = ""
-		if user_input == "w": direction = "up"
-		if user_input == "a": direction = "left"
-		if user_input == "s": direction = "down"
-		if user_input == "d": direction = "right"
-		game.move(direction)
+game = Game()
+agent = Agent(gamma=.99, epsilon = 1.0, batch_size=64,
+				n_actions=4, eps_end=0.01, input_dims=[16],
+				lr=0.1)
+scores, eps_history = [],[]
+avg_scores = []
+n_games = 2500
+for i in range(n_games):
+	game.reset()
+	score = 0
+	done = False
+	observation = game.get_state()
+	#print("observation: ", observation)
+	while not done:
+		action = agent.choose_action(observation)
+		game.move(game.actions[action])
 		game.update()
-		print("Another Move?: ", game.another_move)
-		print("Last move: ", direction)
-		game.print_board()
+		observation_,reward,done = game.get_info()
+		score+=reward
+		agent.store_transition(observation, action, reward, observation_,done)
+		agent.learn()
+		observation = observation_
+	game.print_board()
+	scores.append(game.score)
+	eps_history.append(agent.epsilon)
 
-main()
+	avg_score = np.mean(scores[-100:])
+	print("i: ", i, "avg_score: ", avg_score, " epsilon: ", agent.epsilon)
+
+plt.plot(avg_scores)
+plt.show()
+
+
+
